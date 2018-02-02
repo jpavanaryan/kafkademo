3ff6d5e75eda5a391459791ca9a64b0a2a112204
Pavan Jadda <jpavanaryan@gmail.com>
2018-02-02 17:37:15 -0500

    Regenerated

--- projects/src/main/java/com/projects/config/MessagingConfiguration.java
+++ projects/src/main/java/com/projects/config/MessagingConfiguration.java
@@ -3,17 +3,13 @@ package com.projects.config;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
-import com.projects.messaging.ConsumerChannel;
-import com.projects.messaging.ProducerChannel;
 /**
  * Configures Spring Cloud Stream support.
  *
- * See
- * http://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/
+ * See http://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/
  * for more information.
  */
-@EnableBinding(value ={ Source.class, ProducerChannel.class, ConsumerChannel.class })
-public class MessagingConfiguration
-{
+@EnableBinding(value = {Source.class})
+public class MessagingConfiguration {
 
 }
